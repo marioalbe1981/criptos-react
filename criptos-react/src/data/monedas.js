0c39996424c46b8598d741659e8a0475a9304637
marioalbe1981 <73264376+marioalbe1981@users.noreply.github.com>
2022-05-31 14:52:31 -0300

    Add files via upload

---
+++ criptos-react/src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'MXN', nombre: 'Pesos Mexicanos'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Estarlina'},
+    {id: 'ARS', nombre: 'Pesos Argentinos'},
+]
+
+export {
+    monedas
+}
